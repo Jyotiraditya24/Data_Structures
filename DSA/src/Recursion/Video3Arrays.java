11d9e2f6eec04216006c3b9f49179929c2e39238
jyotiraditya <jyotiraditya24@gmail.com>
2023-05-11 23:59:34 +0530

    Video 3 done important concept learned herre

--- DSA/src/Recursion/Video3Arrays.java
+++ DSA/src/Recursion/Video3Arrays.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 public class Video3Arrays {
     public static void main(String[] args) {
-        int[] arr = {-10,0,10,20};
+        int[] arr = {-10,0,0,0,10,20};
         System.out.println(sorted(arr,0));
         System.out.println(linear(arr,0,-100));
+        System.out.println(findAllIndex(arr,0,40));
     }
 
     public static boolean sorted(int[] arr, int pointer) {
@@ -23,6 +24,15 @@ public class Video3Arrays {
         return arr[pointer]!=target && linear(arr,pointer+1,target);
     }
     public static ArrayList<Integer> findAllIndex(int[]arr,int pointer,int target){
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        if(pointer == arr.length){
+            return new ArrayList<>();
+        }
+        if(arr[pointer] == target){
+            list.add(pointer);
+        }
+        ArrayList<Integer> list2 =  findAllIndex(arr,pointer+1,target);
+         list.addAll(list2);
+         return list;
     }
 }
