68b2bca113574e73d8aa05e643e3048d680a29d4
jyotiraditya <jyotiraditya24@gmail.com>
2023-05-24 23:13:57 +0530

    backtrack using matrix all directions

--- DSA/src/backtracking/Main.java
+++ DSA/src/backtracking/Main.java
@@ -151,4 +151,27 @@ public class Main {
         }
         maze[row][col] = true;
     }
+    static void backTrackMatrix(String p,boolean[][]maze,int row,int col){
+        if(row == maze.length-1 && col == maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        if(!maze[row][col]){
+            return;
+        }
+        maze[row][col] = false;
+        if(row<maze.length-1 ){
+            backTrackMatrix(p+"D",maze,row+1,col);
+        }
+        if(col< maze[0].length -1 ){
+            backTrackMatrix(p+"R",maze,row,col+1);
+        }
+        if(row>0){
+            backTrackMatrix(p+"U",maze,row-1,col);
+        }
+        if(col>0){
+            backTrackMatrix(p+"L",maze,row,col-1);
+        }
+        maze[row][col] = true;
+    }
 }
