4364d8ae68239b996c0a214d9e0704876c7f3b93
jyotiraditya <jyotiraditya24@gmail.com>
2023-05-24 13:21:31 +0530

    maze problem without backtracking

--- DSA/src/backtracking/Main.java
+++ DSA/src/backtracking/Main.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(count(3,3));
-        path(" ",3,3);
-        System.out.println(pathInArrayListDiagonal("",3,3));
+//        System.out.println(count(3,3));
+//        path(" ",3,3);
+//        System.out.println(pathInArrayListDiagonal("",3,3));
+//        pathWithObstacles("",0,0);
+        boolean [][]board = {
+                {true, true, true,},
+                {true,false,true},
+                {true,true,true},
+        };
+        pathWithObstacles2("",board,0,0);
     }
 
     static int count (int row , int col){
@@ -84,5 +91,39 @@ public class Main {
             ans.addAll(pathInArrayListDiagonal(p+"DI",row-1,col-1));
         }
         return ans;
+    }
+
+
+    static void pathWithObstacles(String p, int row, int col){
+        if(row == 1 && col==1){
+            return;
+        }
+        if(row==2 && col ==2){
+            System.out.println(p);
+            return;
+        }
+
+        if(row < 2){
+            pathWithObstacles(p+"D",row+1,col);
+        }
+        if(col<2){
+            pathWithObstacles(p+"R",row,col+1);
+        }
+    }
+    static void pathWithObstacles2(String p, boolean [][]maze,int row, int col){
+        if(!maze[row][col]){
+            return; /* if false then return*/
+        }
+        if(row == maze.length-1 && col == maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(row < maze.length-1){
+            pathWithObstacles(p+"D",row+1,col);
+        }
+        if(col<maze[0].length -1 ){
+            pathWithObstacles(p+"R",row,col+1);
+        }
     }
 }
