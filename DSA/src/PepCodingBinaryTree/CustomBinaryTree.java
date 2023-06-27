aca5159dfb922bed4239c7cb188849ceb818d51f
jyotiraditya <jyotiraditya24@gmail.com>
2023-06-27 07:55:56 +0530

    Video 1 Done

--- DSA/src/PepCodingBinaryTree/CustomBinaryTree.java
+++ DSA/src/PepCodingBinaryTree/CustomBinaryTree.java
@@ -1,4 +1,26 @@
 package PepCodingBinaryTree;
 
 public class CustomBinaryTree {
+
+
+
+
+
+
+
+    private static class Node{
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data){
+            this.data = data;
+        }
+        Node(int data,Node left,Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
+
